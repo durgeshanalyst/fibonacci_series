afcdbe1813ad2b01028fc25a92d5a88bbfeacec1
Durgesh Kumar <durgeshkumar7b@gmail.com>
2023-09-12 12:54:12 +0530

    Create fibonacci_series.py

---
+++ fibonacci_series.py
@@ -0,0 +1,19 @@
+n=int(input('Enter number: '))
+x=0
+y=1
+z=x+y
+t=n# copy for not destring the main
+while t <=z:
+    print(z)
+    x=y
+    y=z
+    z=x+y
+
+'''Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. 
+So, the first few numbers in the Fibonacci series look like this:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+Each number in the series (starting from the third number) is the sum of the two numbers immediately before it.
+ For example, 2 is the sum of 1 and 1, 3 is the sum of 1 and 2, 5 is the sum of 2 and 3, and so on. 
+ This pattern continues indefinitely, creating a unique sequence of numbers with many interesting mathematical properties.'''
